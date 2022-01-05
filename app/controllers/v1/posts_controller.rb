dd75a2ab7a96ffa111183e7e9abfe90b4374662e
kojikoji <rubypra0804@gmail.com>
2022-01-06 07:55:11 +0900

    showコントローラ

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,10 +1,12 @@
 class V1::PostsController < ApplicationController
+  before_action :set_post, only: %i[show update destroy]
   def index
     posts = Post.all
     render json: posts
   end
 
   def show
+    render json: @post
   end
 
   def create
@@ -14,5 +16,10 @@ class V1::PostsController < ApplicationController
   end
 
   def destroy
+  end
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
